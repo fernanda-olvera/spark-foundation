2e8a79fd419ccf880e36a7f56d722108ef5a3eac
Fernanda Olvera <fer@SPARK-004.localdomain>
2019-11-06 08:05:44 -0800

    added BS and stylesheet

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Task
 
 def index(request):
-    return render(request, 'todo_list/index.html')
+    tasks=Task.objects.all()
+    return render(request, 'todo_list/index.html', {'tasks':tasks})
