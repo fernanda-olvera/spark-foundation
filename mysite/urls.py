c24ba4d02316d1536f28e0057353151729257511
Fernanda Olvera <fernanda.olvera@techsparks.com>
2019-11-07 11:16:11 -0800

    Login form and signup form works

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('todo_list', include('todo_list.urls')),
+    path('todo_list/', include('todo_list.urls')),
+    path('todo_list/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
--- todo_list/views.py
+++ todo_list/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
+# from django.contrib.auth import login as auth_login
+from django.contrib.auth import authenticate
+# from django.contrib.auth.forms import AuthenticationForm
 from .models import Task
 
 from .forms import SignUpForm
@@ -9,8 +11,19 @@ def index(request):
     tasks=Task.objects.all()
     return render(request, 'todo_list/index.html', {'tasks':tasks})
 
-def login(request):
-    return render(request, 'todo_list/login.html', {})
+# def login(request):
+#     if request.method == 'POST':
+#         form=AuthenticationForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             username=form.cleaned_data.get('username')
+#             raw_password=form.cleaned_data.get('password')
+#             user=authenticate(username=username, password=raw_password)
+#             auth_login(request, user)
+#             return redirect('index')  
+#     else:
+#         form=AuthenticationForm()      
+#     return render(request, 'todo_list/login.html', {'form':form})
 
 def signup(request):
     if request.method == 'POST':
@@ -20,8 +33,8 @@ def signup(request):
             username=form.cleaned_data.get('username')
             raw_password=form.cleaned_data.get('password1')
             user=authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect('home')
+            # auth_login(request, user)
+            return redirect('login')
     else:
         form=SignUpForm()
-    return render(request, 'todo_list/signup.html',{'form':form})
+    return render(request, 'todo_list/signup.html', {'form':form})
