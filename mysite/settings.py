1c32f7055f11cbcdc69ad0b83a7e4727c1e13f31
Fernanda Olvera <fernanda.olvera@techsparks.com>
2019-11-13 18:23:56 -0800

    Added template for password reset 1

--- mysite/settings.py
+++ mysite/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +123,6 @@ STATIC_ROOT=os.path.join(BASE_DIR, 'static')
 
 LOGIN_REDIRECT_URL = 'index'
 LOGOUT_REDIRECT_URL = 'index'
+
+EMAIL_BACKEND="django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH=os.path.join(BASE_DIR,"sent_emails")
--- todo_list/urls.py
+++ todo_list/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
 from .forms import LogInForm
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, PasswordResetView
 
 urlpatterns=[
     path('', views.index, name='index'),
     path('signup', views.signup, name='signup'),
     path('login/', LoginView.as_view(template_name='registration/login.html', authentication_form=LogInForm), name='login'),
+    path('password_reset/', PasswordResetView.as_view(template_name='todo_list/password_reset_form.html'), name='password_reset'),
 ]
