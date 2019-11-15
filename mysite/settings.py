ce3a919c6dc555288fe527a79805eaf1bd203da2
Fernanda Olvera <fernanda.olvera@techsparks.com>
2019-11-15 14:23:21 -0800

    Added django-heroku to requirements

--- mysite/settings.py
+++ mysite/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 LOGIN_REDIRECT_URL = 'index'
 LOGOUT_REDIRECT_URL = 'index'
