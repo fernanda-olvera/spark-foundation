42aab73f5d77e39fc0fbc7b14c7deb922a08ff81
Fernanda Olvera <fernanda.olvera@techsparks.com>
2019-11-19 12:25:53 -0800

    Added verification email

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,6 +20,6 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     path('', include('todo_list.urls')),
     path('', include('django.contrib.auth.urls')),
-    path('', include('allauth.urls')),
+    path('accounts/', include('allauth.urls')),
     path('admin/', admin.site.urls),
 ]
